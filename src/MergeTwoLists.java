aa9f80b9d7c29d42713ddc3432236eb169a67503
hchc6480-k <hchc6480@gmail.com>
2023-04-13 17:46:16 +0900

    Merge Two Lists

---
+++ src/MergeTwoLists.java
@@ -0,0 +1,50 @@
+public class MergeTwoLists {
+    /**
+     *
+     * Input: l1 = [2,4,3], l2 = [5,6,4]
+     * Output: [7,0,8]
+     * Explanation: 342 + 465 = 807
+     */
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        //l1.next = new ListNode(2);
+
+        ListNode node = new ListNode();
+        ListNode result = node;
+        int sum = 0;
+
+        while (l1 != null || l2 != null || sum > 0) {
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            node.next = new ListNode(sum % 10);
+            sum = sum / 10;
+
+            node = node.next;
+        }
+
+        result = result.next; // default 0 제거
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+
+    }
+
+
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
