f23e42130a060e79634dd5638f7444aa98bca4b3
hchc6480-k <hchc6480@gmail.com>
2023-04-18 17:46:22 +0900

    Merge Two Sorted Lists

---
+++ src/MergeTwoSortedLists.java
@@ -0,0 +1,57 @@
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        ListNode node = new ListNode();
+        ListNode result = node;
+
+        /*while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                result.next = l1;
+                l1 = l1.next;
+            } else {
+                result.next = l2;
+                l2 = l2.next;
+            }
+
+            result = result.next;
+        }
+
+        result.next = l1 == null ? l2 : l1;
+
+        node = node.next;
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }*/
+
+        ListNode test = mergeTwoSortedList(l1, l2);
+        while (test != null) {
+            System.out.println(test.val);
+            test = test.next;
+        }
+
+    }
+    /** solution */
+    public static ListNode mergeTwoSortedList(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoSortedList(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoSortedList(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
